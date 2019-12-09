58e4073244b4c426eaf5326e9c79229eff7f9683
ElB0ludo <boluelbot@gmail.com>
2019-12-09 07:03:59 +0100

    Day 9.

---
+++ Day_9/two.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Advent of Code 2019 - Day 9
+
+# BOOST
+import sys
+sys.path.append("../python_modules/custom")
+from intcomputer import Intcomputer
+
+com = Intcomputer(list(map(int, open("boost.txt", "r").read().split(","))))
+
+com.input([2])
+com.run()
+
+print("Output:", com.output()[0])
--- python_modules/custom/intcomputer.py
+++ python_modules/custom/intcomputer.py
@@ -3,79 +3,85 @@
 # Wrote after Day 7. Intcomputer puzzles were getting annoying.
 
 class Intcomputer:
-	__mem__ = []
-	__pc__ = 0
-	__indata__ = []
-	__outdata__ = []
-	__states__ = "RIF"
-	__ps__ = __states__[0]
+	__mem = []
+	__pc = 0
+	__indata = []
+	__outdata = []
+	__states = "RIF"
+	__ps = __states[0]
+	__rb = 0
 
 	def __init__(self, program):
-		self.__mem__ = program[:]
-		self.__indata__ = []
-		self.__outdata__ = []
+		self.__mem = program[:]
+		self.__mem.extend([0] * len(program))
+		self.__indata = []
+		self.__outdata = []
 
 	def input(self, e):
-		self.__indata__.extend(e)
-		if self.__ps__ == 'I':
-			self.__ps__ = self.__states__[0]
+		self.__indata.extend(e)
+		if self.__ps == 'I':
+			self.__ps = self.__states[0]
 			self.run()
 
 	def output(self):
-		r = self.__outdata__[:]
-		self.__outdata__ = []
+		r = self.__outdata[:]
+		self.__outdata = []
 		return r
 
-	def __sum__(self, i):
-		self.__mem__[i.dst] = self.__mem__[i.srcA] + self.__mem__[i.srcB]
-		self.__pc__ -=- 4
+	def __sum(self, i):
+		self.__mem[i.dst] = self.__mem[i.srcA] + self.__mem[i.srcB]
+		self.__pc -=- 4
 
-	def __prod__(self, i):
-		self.__mem__[i.dst] = self.__mem__[i.srcA] * self.__mem__[i.srcB]
-		self.__pc__ -=- 4
+	def __prod(self, i):
+		self.__mem[i.dst] = self.__mem[i.srcA] * self.__mem[i.srcB]
+		self.__pc -=- 4
 
-	def __input__(self, i):
-		if len(self.__indata__) > 0:
-			self.__mem__[i.srcA] = self.__indata__.pop(0)
-			self.__pc__ -=- 2
+	def __input(self, i):
+		if len(self.__indata) > 0:
+			self.__mem[i.srcA] = self.__indata.pop(0)
+			self.__pc -=- 2
 		else:
-			self.__ps__ = self.__states__[1]
+			self.__ps = self.__states[1]
 			return 1
 
-	def __output__(self, i):
-		self.__outdata__.append(self.__mem__[i.srcA])
-		self.__pc__ -=- 2
+	def __output(self, i):
+		self.__outdata.append(self.__mem[i.srcA])
+		self.__pc -=- 2
 
-	def __jumpz__(self, i):
-		if self.__mem__[i.srcA]: self.__pc__ = self.__mem__[i.srcB]
-		else: self.__pc__ -=- 3
+	def __jumpz(self, i):
+		if self.__mem[i.srcA]: self.__pc = self.__mem[i.srcB]
+		else: self.__pc -=- 3
 
-	def __jumpnz__(self, i):
-		if not self.__mem__[i.srcA]: self.__pc__ = self.__mem__[i.srcB]
-		else: self.__pc__ -=- 3
+	def __jumpnz(self, i):
+		if not self.__mem[i.srcA]: self.__pc = self.__mem[i.srcB]
+		else: self.__pc -=- 3
 
-	def __slt__(self, i):
-		self.__mem__[i.dst] = int(self.__mem__[i.srcA] < self.__mem__[i.srcB])
-		self.__pc__ -=- 4
+	def __slt(self, i):
+		self.__mem[i.dst] = int(self.__mem[i.srcA] < self.__mem[i.srcB])
+		self.__pc -=- 4
 
-	def __seq__(self, i):
-		self.__mem__[i.dst] = int(self.__mem__[i.srcA] == self.__mem__[i.srcB])
-		self.__pc__ -=- 4
+	def __seq(self, i):
+		self.__mem[i.dst] = int(self.__mem[i.srcA] == self.__mem[i.srcB])
+		self.__pc -=- 4
 
-	def __halt__(self, i):
-		self.__ps__ = self.__states__[2]
+	def __rel(self, i):
+		self.__rb += self.__mem[i.srcA]
+		self.__pc -=- 2
+
+	def __halt(self, i):
+		self.__ps = self.__states[2]
 		return 1
 
-	__exec__ = {1: __sum__, 2: __prod__, 3: __input__, 4: __output__, 5: __jumpz__, 6: __jumpnz__, 7: __slt__, 8: __seq__, 99: __halt__}
+	__exec = {1: __sum, 2: __prod, 3: __input, 4: __output, 5: __jumpz, 6: __jumpnz, 7: __slt, 8: __seq, 9: __rel, 99: __halt}
 
 	def step(self):
-		if self.__ps__ == 'R':
-			instruction = Intinstruction(self.__mem__, self.__pc__)
-			return 1 if self.__exec__[instruction.opcode](self, instruction) else 0
+		if self.__ps == 'R':
+			instruction = Intinstruction(self.__mem, self.__pc, self.__rb)
+			return 1 if self.__exec[instruction.opcode](self, instruction) else 0
 		return 1
 
 	def state(self):
-		return self.__ps__
+		return self.__ps
 
 	def run(self):
 		while not self.step(): pass
@@ -88,21 +94,21 @@ class Intinstruction:
 	srcB = 0
 	dst = 0
 
-	def __init__(self, program, i):
+	def __init__(self, program, i, rel):
 		self.opcode = program[i] % 100
-		params = self.__getParams__(program[i] // 100)
+		params = self.__getParams(program[i] // 100)
 
 		while len(program) < (i + 3) + 1:
 			program.append(0)
 
-		self.srcA = i + 1 if params[0] else program[i + 1]
-		self.srcB = i + 2 if params[1] else program[i + 2]
-		self.dst  = i + 3 if params[2] else program[i + 3]
+		self.srcA = program[i + 1] + rel if params[0] == 2 else i + 1 if params[0] == 1 else program[i + 1]
+		self.srcB = program[i + 2] + rel if params[1] == 2 else i + 2 if params[1] == 1 else program[i + 2]
+		self.dst  = program[i + 3] + rel if params[2] == 2 else i + 3 if params[2] == 1 else program[i + 3]
 
 	@staticmethod
-	def __getParams__(params):
+	def __getParams(params):
 		d = params // 100
-		B = params // 10 - d*10
-		A = params - d*100 - B*10
+		B = params // 10 - 10*d
+		A = params - 100*d - 10*B
 
 		return(A, B, d)
